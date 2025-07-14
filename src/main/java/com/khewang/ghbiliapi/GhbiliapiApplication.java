db1584c70bd1440dd2682912ce6c105a1eaa6a2a
Hyouka45 <khewang212@gmail.com>
2025-07-14 21:02:21 +0545

    first commit

---
+++ src/main/java/com/khewang/ghbiliapi/GhbiliapiApplication.java
@@ -0,0 +1,13 @@
+package com.khewang.ghbiliapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhbiliapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhbiliapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khewang/ghbiliapi/GhbiliapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khewang.ghbiliapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhbiliapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
