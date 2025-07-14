f1702353587e34924d043ed72df6e3f9eeabfc69
Hyouka45 <khewang212@gmail.com>
2025-07-14 21:33:17 +0545

    FeignConfig done

--- src/main/java/com/khewang/ghbiliapi/GhbiliapiApplication.java
+++ src/main/java/com/khewang/ghbiliapi/GhbiliapiApplication.java
@@ -2,8 +2,10 @@ package com.khewang.ghbiliapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GhbiliapiApplication {
 
 	public static void main(String[] args) {
